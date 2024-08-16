17711afd3be92339b2fd4f9cd2531416415d2052
lxw-bfw <1143167344@qq.com>
2024-08-16 12:39:17 +0800

    feat: 搭建统一代码风格的一套自动化流程

---
+++ .stylelintrc.mjs
@@ -0,0 +1,20 @@
+export default {
+  extends: 'stylelint-config-standard-scss',
+  plugins: ['stylelint-scss'],
+  rules: {
+    'at-rule-no-unknown': null, // 先禁用默认的规则
+    'scss/at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'tailwind', // 允许 @tailwind 指令
+          'apply', // 允许 @apply 指令
+          'variants', // 允许 @variants 指令
+          'responsive', // 允许 @responsive 指令
+          'screen', // 允许 @screen 指令
+        ],
+      },
+    ],
+  },
+  customSyntax: 'postcss-scss',
+}
